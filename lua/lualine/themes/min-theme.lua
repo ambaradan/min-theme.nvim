c879f0f7681e8193cccea9393e415275ef1cddf9
ambaradan <ambaradan@proton.me>
2025-05-02 11:07:48 +0200

    feat: setup lualine 'min-theme'

--- lua/lualine/themes/min-theme.lua
+++ lua/lualine/themes/min-theme.lua
@@ -1,38 +1,52 @@
 local colors = require("min-theme.colors")
 local config = require("min-theme.config")
+local utils = require("min-theme.utils")
+
 local min = {}
 
 local bg = config.transparent and "NONE" or colors.bg
 
 min.normal = {
-	a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
-	b = { bg = bg, fg = colors.comment },
-	c = { bg = bg, fg = colors.bg },
+	a = { bg = utils.shade(colors.yellowDark, 0.80), fg = colors.bg, gui = "bold" },
+	b = { bg = utils.shade(colors.bgDark, 0.85), fg = colors.fg },
+	c = { bg = utils.shade(colors.bgDark, 0.95), fg = colors.bg },
+	x = { bg = utils.shade(colors.bgDark, 0.95), fg = colors.fgDisabled },
+	y = { bg = utils.shade(colors.bgDark, 0.95), fg = colors.fgDisabled },
 }
 
 min.insert = {
-	a = { bg = colors.red, fg = colors.bg, gui = "bold" },
-	b = { bg = bg, fg = colors.comment },
+	a = { bg = utils.shade(colors.redDark, 0.80), fg = colors.bg, gui = "bold" },
+	b = { bg = utils.shade(colors.bgDarker, 0.95), fg = colors.fg, gui = "bold" },
+	x = { bg = utils.shade(colors.bgDark, 0.95), fg = colors.fg },
+	y = { bg = utils.shade(colors.bgDark, 0.95), fg = colors.fgDisabled },
 }
 
 min.terminal = {
-	a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-	b = { bg = bg, fg = colors.comment },
+	a = { bg = utils.shade(colors.green, 0.80), fg = colors.bg, gui = "bold" },
+	b = { bg = utils.shade(colors.bgDarker, 0.95), fg = colors.fg },
+	x = { bg = utils.shade(colors.bgDark, 0.95), fg = colors.comment },
+	y = { bg = utils.shade(colors.bgDark, 0.95), fg = colors.fgDisabled },
 }
 
 min.command = {
-	a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-	b = { bg = bg, fg = colors.comment },
+	a = { bg = utils.shade(colors.green, 0.80), fg = colors.bg, gui = "bold" },
+	b = { bg = utils.shade(colors.bgDarker, 0.95), fg = colors.fg },
+	x = { bg = utils.shade(colors.bgDark, 0.95), fg = colors.comment },
+	y = { bg = utils.shade(colors.bgDark, 0.95), fg = colors.fgDisabled },
 }
 
 min.visual = {
-	a = { bg = colors.purple, fg = colors.bg, gui = "bold" },
-	b = { bg = bg, fg = colors.comment },
+	a = { bg = utils.shade(colors.purple, 0.80), fg = colors.bg, gui = "bold" },
+	b = { bg = utils.shade(colors.bgDarker, 0.95), fg = colors.fg, gui = "bold" },
+	x = { bg = utils.shade(colors.bgDark, 0.95), fg = colors.comment },
+	y = { bg = utils.shade(colors.bgDark, 0.95), fg = colors.fgDisabled },
 }
 
 min.replace = {
-	a = { bg = colors.orange, fg = colors.bg, gui = "bold" },
-	b = { bg = bg, fg = colors.comment },
+	a = { bg = utils.shade(colors.orange, 0.80), fg = colors.bg, gui = "bold" },
+	b = { bg = utils.shade(colors.bgDarker, 0.95), fg = colors.fg, gui = "bold" },
+	x = { bg = utils.shade(colors.bgDark, 0.95), fg = colors.comment },
+	y = { bg = utils.shade(colors.bgDark, 0.95), fg = colors.fgDisabled },
 }
 
 min.inactive = {
